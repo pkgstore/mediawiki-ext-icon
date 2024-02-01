611194645153327fba36c549aed075a40921699a
KaiKimera <mail@kai.kim>
2024-02-01 04:31:21 +0000

    2024-02-01 04:31:21

--- MW_EXT_Icon.i18n.magic.php
+++ MW_EXT_Icon.i18n.magic.php
@@ -6,14 +6,14 @@
  * @file
  * @ingroup Extensions
  * @license MIT
- * @author iHub TO <https://ihub.to>
+ * @author Kai Kimera <https://kai.kim>
  */
 $magicWords = [];
 
 /**
  * English.
  *
- * @author iHub TO <https://ihub.to>
+ * @author Kai Kimera <https://kai.kim>
  */
 $magicWords['en'] = [
   'icon' => [0, 'icon'],
@@ -22,7 +22,7 @@ $magicWords['en'] = [
 /**
  * Russian.
  *
- * @author iHub TO <https://ihub.to>
+ * @author Kai Kimera <https://kai.kim>
  */
 $magicWords['ru'] = [
   'icon' => [0, 'icon'],
